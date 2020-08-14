c7429fe51a0c8ac182c56d6d00b78979efac7765
jhg0675 <tjdtlrud4562@gmail.com>
2020-08-14 11:23:53 +0900

    2020_08_14
    스크립터블 오브젝트 활용 예시

---
+++ ScriptableObject/Assets/Scripts/Zombie.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    [SerializeField]
+    private ZombieData zombieData;
+    public ZombieData ZombieData { set { zombieData = value; } }
+
+    public void WatchZombieInfo()
+    {
+        Debug.Log("좀비 이름 :: " + zombieData.ZombieName);
+        Debug.Log("좀비 체력 :: " + zombieData.Hp);
+        Debug.Log("좀비 공격력 :: " + zombieData.Damage);
+        Debug.Log("좀비 시야 :: " + zombieData.SightRange);
+        Debug.Log("좀비 이동속도 :: " + zombieData.MoveSpeed);
+    }
+}
---
+++ ScriptableObject/Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ZombieType { Normal , Power, Sensor , Speed, Tank }
+
+public class ZombieSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<ZombieData> zombieDatas;
+
+    [SerializeField]
+    private GameObject zombiePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < zombieDatas.Count; i++)
+        {
+            var zombie = SpawnZombie((ZombieType)i);
+            zombie.WatchZombieInfo();
+        }
+    }
+
+    public Zombie SpawnZombie(ZombieType type)
+    {
+        var newZombie = Instantiate(zombiePrefab).GetComponent<Zombie>();
+        newZombie.ZombieData = zombieDatas[(int)type];
+        return newZombie;
+    }
+}
